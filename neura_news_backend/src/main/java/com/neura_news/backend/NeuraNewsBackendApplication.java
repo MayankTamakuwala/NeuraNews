963dfa4d7d064b94e9b4b6c99a19584d475dda3d
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-09-29 03:24:40 -0400

    NeuraNews Backend Init
    Co-Authored-By: DhruvGorasiya <80987415+DhruvGorasiya@users.noreply.github.com>

---
+++ neura_news_backend/src/main/java/com/neura_news/backend/NeuraNewsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neura_news.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuraNewsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuraNewsBackendApplication.class, args);
+	}
+
+}
---
+++ neura_news_backend/src/test/java/com/neura_news/backend/NeuraNewsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neura_news.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuraNewsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
